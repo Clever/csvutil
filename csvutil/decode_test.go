b9416800ae7b32deafd8a4757c901dd643597679
Nate Brennand <nate.brennand@clever.com>
2016-06-24 17:53:28 +0000

    add tests for NewDecoder

--- csvutil/decode_test.go
+++ csvutil/decode_test.go
@@ -1,6 +1,7 @@
 package csvutil
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -18,21 +19,65 @@ func TestNewDecoder(t *testing.T) {
 		{
 			msg: "simple struct w/ string field",
 			s: struct {
-				StrField int `csv:"string"`
+				StrField string `csv:"string"`
 			}{},
 			csvFile:    "string\ntest\ntest2",
 			fieldOrder: []string{"string"},
+		},
+		{
+			msg: "simple struct w/ two fields",
+			s: struct {
+				StrField     string `csv:"string"`
+				IntegerField int    `csv:"integer,required"`
+			}{},
+			csvFile:    "string,integer\ntest,1\ntest,2",
+			fieldOrder: []string{"string", "integer"},
+		},
+		{
+			msg: "simple struct w/ two fields, and extraneous CSV headers",
+			s: struct {
+				StrField     string `csv:"string"`
+				IntegerField int    `csv:"integer,required"`
+			}{},
+			csvFile:    "string,c1,c2,integer\ntest,1\ntest,2",
+			fieldOrder: []string{"string", "integer"},
+		},
+		{
+			msg: "simple struct w/ two fields, order opposite of headers",
+			s: struct {
+				IntegerField int    `csv:"integer,required"`
+				StrField     string `csv:"string"`
+			}{},
+			csvFile:    "string,integer\ntest,1\ntest,2",
+			fieldOrder: []string{"integer", "string"},
+		},
+		{
+			msg: "error when missing required field",
+			s: struct {
+				IntegerField int    `csv:"integer,required"`
+				StrField     string `csv:"string"`
+			}{},
+			csvFile: "string\ntest\ntest2",
+			err:     fmt.Errorf("column 'integer' required but not found"),
+		},
+		{
+			msg: "error when csv file has header twice",
+			s: struct {
+				StrField string `csv:"string"`
+			}{},
+			csvFile: "string,test,string\n",
+			err:     fmt.Errorf("saw header column 'string' twice, CSV headers must be unique"),
 		},
 	}
 
 	for _, s := range specs {
 		d, err := NewDecoder(strings.NewReader(s.csvFile), s.s)
-		if assert.Equal(t, s.err, err) {
-			fields := make([]string, d.numColumns)
+		if assert.Equal(t, s.err, err, s.msg) && s.err == nil {
+			fields := make([]string, len(d.mappings))
 			for i, m := range d.mappings {
 				fields[i] = m.fieldName
 			}
-			assert.Equal(t, s.fieldOrder, fields)
+			assert.Equal(t, s.fieldOrder, fields, s.msg)
 		}
 	}
 }
